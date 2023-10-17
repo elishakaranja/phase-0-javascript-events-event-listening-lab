5b48df3b076a42e134a2d0ef45bc98f10a7968ab
elishakaranja <145563815+elishakaranja@users.noreply.github.com>
2023-10-18 00:22:17 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
+
+const sinon = require('sinon');
+
+describe("index.js", () => {
+  let input;
+
+  beforeEach(function() {
+    input = document.getElementById('input');
+    sinon.spy(input, 'addEventListener');
+  })
+
+  it("binds an event listener in addingEventListener()", () => {
+    addingEventListener();
+    expect(input.addEventListener.called).to.be.true;
+  })
+})
+
 function addingEventListener() {
-}
+    // Getting the input element by ID
+    const input = document.getElementById('input');
+  
+    // Adding an event listener 
+    input.addEventListener('click', function () {
+      // Your event handler logic here
+    });
+  }
+  
+  // Call the outer function to set up the event listener
+  addingEventListener();
